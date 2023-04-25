d36d5c53e1eb46ccfc1e62be36021660fed2c8cb
Umbrason <45980080+Umbrason@users.noreply.github.com>
2023-04-25 19:52:12 +0200

    fix DanceDelay field names

--- Assets/Script/NPC/NPC.cs
+++ Assets/Script/NPC/NPC.cs
@@ -6,14 +6,14 @@ public class NPC : MonoBehaviour
     private DanceMoveExecutor cached_dme;
     private DanceMoveExecutor DanceMoveExecutor => cached_dme ??= GetComponent<DanceMoveExecutor>();
 
-    public float minDanceFrequency, maxDanceFrequency;
+    public float minDanceDelay, maxDanceDelay;
     private float nextMoveTime;
 
     void Update()
     {
         
         if (nextMoveTime > Time.time) return;
-        nextMoveTime = Time.time + Random.Range(minDanceFrequency, maxDanceFrequency);
+        nextMoveTime = Time.time + Random.Range(minDanceDelay, maxDanceDelay);
         if (DanceMoveExecutor.IsDancing) return;
         var danceMoveIndex = Random.Range(0, DanceMoveExecutor.AvailableMoves.Length);
         DanceMoveExecutor.UseMove(DanceMoveExecutor.AvailableMoves[danceMoveIndex]);

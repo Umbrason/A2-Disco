728079a0a7a577663779f63caad147b5e4afe27e
Umbrason <45980080+Umbrason@users.noreply.github.com>
2023-04-25 20:11:12 +0200

    Implement Light Spawner

---
+++ Assets/Script/Lights/LightSpawner.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class LightSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject Prefab;
+    [SerializeField] private Camera SpawnRegionCamera;
+
+    public void Spawn()
+    {
+        var instance = Instantiate(Prefab);
+        var min = SpawnRegionCamera.cameraToWorldMatrix.MultiplyPoint(Vector3.zero)._xy();
+        var max = SpawnRegionCamera.cameraToWorldMatrix.MultiplyPoint(Vector3.one)._xy();
+        instance.transform.position = new Vector2(Random.Range(min.x, max.x), Random.Range(min.y, max.y));
+    }
+}

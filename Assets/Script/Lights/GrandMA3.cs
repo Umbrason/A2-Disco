183f8f174af7938fc2b39cca5f5b974758f5bb5f
Umbrason <45980080+Umbrason@users.noreply.github.com>
2023-04-25 21:17:39 +0200

    Make GrandMA3 extend MonoBehaviour

--- Assets/Script/Lights/GrandMA3.cs
+++ Assets/Script/Lights/GrandMA3.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
+using UnityEngine;
 
-public class GrandMA3
+public class GrandMA3 : MonoBehaviour
 {
     public readonly static List<DiscoLight> lights = new();
     public static ILightingEffect ActiveEffect;

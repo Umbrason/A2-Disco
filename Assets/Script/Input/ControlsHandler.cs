5c11594e2aae25ea5ba85724fabbe60a2d89a6c6
Jartreg <Jartreg@users.noreply.github.com>
2023-04-29 19:28:17 +0200

    Add support for dance move selection

--- Assets/Script/Input/ControlsHandler.cs
+++ Assets/Script/Input/ControlsHandler.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(MovementController))]
+[RequireComponent(typeof(MovementController), typeof(DanceMoveExecutor))]
 public class ControlsHandler : MonoBehaviour, Controls.IControlsMapActions
 {
     private Controls Input;
     private MovementController cached_mc;
     private MovementController MovementController => cached_mc ??= GetComponent<MovementController>();
 
+    private DanceMoveExecutor CachedDMExecutor;
+    private DanceMoveExecutor DanceMoveExecutor => CachedDMExecutor ??= GetComponent<DanceMoveExecutor>();
 
     public void Start()
     {
@@ -20,5 +22,15 @@ public class ControlsHandler : MonoBehaviour, Controls.IControlsMapActions
     public void OnMove(InputAction.CallbackContext context)
     {
         MovementController.MovementInput = context.ReadValue<Vector2>();
-    }    
+    }
+
+    public void OnSelectDanceMove(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        var index = (int)(context.ReadValue<float>()) - 1;
+
+        var moves = DanceMoveExecutor.AvailableMoves;
+        if (index < 0 || index >= moves.Length) return;
+        DanceMoveExecutor.UseMove(moves[index]);
+    }
 }

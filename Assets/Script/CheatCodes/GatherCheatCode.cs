30d92068cc0f5dbbf2c3950d96bf2385b9fa4b16
Jartreg <Jartreg@users.noreply.github.com>
2023-04-30 20:43:51 +0200

    Implement GatherCheatCode

---
+++ Assets/Script/CheatCodes/GatherCheatCode.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GatherCheatCode : MonoBehaviour, ICheatCode
+{
+    public string Name => "gather";
+
+    [SerializeField]
+    private GameObject player;
+
+    void Start()
+    {
+        DanceMoveExecutor.StartDancing += OnStartDancing;
+    }
+
+    void OnDestroy()
+    {
+        DanceMoveExecutor.StartDancing -= OnStartDancing;
+    }
+
+    void OnStartDancing(DanceMoveExecutor executor, IDanceMove move)
+    {
+        if (move is ILongDanceMove && Random.value > .6f) Gather(executor.transform.position._xy());
+    }
+
+    public void Execute() => Gather(player.transform.position._xy()); 
+
+    void Gather(Vector2 center, float duration = 2f)
+    {
+        var rootObjects = SceneManager.GetActiveScene().GetRootGameObjects();
+        var targets = rootObjects
+            .SelectMany(obj => obj.GetComponentsInChildren<NPC>(false))
+            .Where(npc => npc.gameObject.activeInHierarchy)
+            .OrderBy(npc => (center - npc.transform.position._xy()).sqrMagnitude)
+            .Take(10)
+            .OrderBy(_ => Random.value)
+            .ToArray();
+
+        var rotationStep = Quaternion.Euler(0, 0, 360f / targets.Length);
+        var offset = Vector2.up * 7;
+
+        foreach(var target in targets)
+        {
+            var targetPos = center + offset;
+            target.JoinGathering(targetPos, duration);
+            offset = rotationStep * offset;
+        }
+    }
+}
--- Assets/Script/NPC/NPC.cs
+++ Assets/Script/NPC/NPC.cs
@@ -1,17 +1,78 @@
+using System.Collections;
 using UnityEngine;
 
-[RequireComponent(typeof(DanceMoveExecutor))]
+[RequireComponent(typeof(DanceMoveExecutor), typeof(MovementController), typeof(VelocityController))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class NPC : MonoBehaviour
 {
     private DanceMoveExecutor cached_dme;
     private DanceMoveExecutor DanceMoveExecutor => cached_dme ??= GetComponent<DanceMoveExecutor>();
 
+    private MovementController cachedMC;
+    private MovementController MC => cachedMC ??= GetComponent<MovementController>();
+
+    private VelocityController cachedVC;
+    private VelocityController VC => cachedVC ??= GetComponent<VelocityController>();
+
+    private BoxCollider2D cachedBC;
+    private BoxCollider2D BC => cachedBC ??= GetComponent<BoxCollider2D>();
+
     public float minDanceDelay, maxDanceDelay;
     private float nextMoveTime;
 
+    private Vector2 home;
+    private float noiseSeed = 0;
+
+    private Coroutine currentGathering;
+
+    void Start()
+    {
+        home = transform.position._xy();
+        noiseSeed = Random.value;
+    }
+
+    void FixedUpdate()
+    {
+        // don't wander home during gatherings
+        if (currentGathering != null)
+        {
+            MC.MovementInput = Vector2.zero;
+            return;
+        }
+
+        var movement = home - transform.position._xy();
+        if (movement.sqrMagnitude < .1f)
+        {
+            MC.MovementInput = Vector2.zero;
+            return;
+        }
+
+        var wanderingFactor = Mathf.Min(movement.sqrMagnitude, 3f) / 3f;
+        var wanderingRotation = Quaternion.Euler(0, 0, 120 * (Mathf.PerlinNoise(Time.time, noiseSeed) - .5f) * wanderingFactor);
+        MC.MovementInput = wanderingRotation * movement;
+        MC.BaseSpeed = 5 * Mathf.PerlinNoise(Time.time, noiseSeed + .4f);
+    }
+
+    public void JoinGathering(Vector2 target, float duration)
+    {
+        if (currentGathering != null) StopCoroutine(currentGathering);
+        currentGathering = StartCoroutine(Gathering(target, duration));
+    }
+
+    private IEnumerator Gathering(Vector2 target, float duration)
+    {
+        BC.enabled = false;
+        yield return VC.MoveDampened(target, .5f);
+        BC.enabled = true;
+        yield return new WaitForSeconds(duration);
+        currentGathering = null;
+    }
+
     void Update()
     {
-        
+        // don't dance during gatherings
+        if (currentGathering != null) return;
+
         if (nextMoveTime > Time.time) return;
         nextMoveTime = Time.time + Random.Range(minDanceDelay, maxDanceDelay);
         if (DanceMoveExecutor.IsDancing) return;

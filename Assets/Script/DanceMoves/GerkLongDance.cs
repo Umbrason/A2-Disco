064407d1e81363b24ea0efaed6f4beacd9928bea
Patrick Gerk <patrick.gerk@ruhr-uni-bochum.de>
2023-04-30 10:42:05 +0200

    Fixed stuff from comments

--- Assets/Script/DanceMoves/GerkLongDance.cs
+++ Assets/Script/DanceMoves/GerkLongDance.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GerkLongDance : MonoBehaviour, ILongDanceMove
+public class GerkLongDance : MonoBehaviour, IDanceMove, ILongDanceMove
 {
     public IEnumerator Perform (Transform target)
     {
@@ -12,43 +12,49 @@ public class GerkLongDance : MonoBehaviour, ILongDanceMove
             {
                 target.position += Vector3.up * (i / 10);
                 target.Rotate((new Vector3(0, 0, .1f) * i));
+                yield return new WaitForSeconds(.02f);
             }
             for (int i = 0; i < 11; i++)
             {
                 target.position += Vector3.right * (i / 10);
                 target.Rotate(new Vector3(0, 0, .1f) * i);
+                yield return new WaitForSeconds(.02f);
             }
             for (int i = 0; i < 11; i++)
             {
                 target.position += Vector3.down * (i / 10);
                 target.Rotate(new Vector3(0, 0, .1f) * i);
+                yield return new WaitForSeconds(.02f);
             }
             for (int i = 0; i < 11; i++)
             {
                 target.position += Vector3.left * (i / 10);
                 target.Rotate(new Vector3(0, 0, .1f) * i);
+                yield return new WaitForSeconds(.02f);
             }
             for (int i = 0; i < 11; i++)
             {
                 target.localScale += new Vector3(.1f, .1f, 0) * i;
+                yield return new WaitForSeconds(.02f);
             }
             for (int i = 0; i < 11; i++)
             {
                 target.localScale -= new Vector3(.1f, .1f, 0) * i;
+                yield return new WaitForSeconds(.02f);
             }
 
             if (repeat % 2 == 0)
             {
                 target.localScale += new Vector3(1f, 0, 0);
                 target.localScale += new Vector3(0, -1f, 0);
+                yield return new WaitForSeconds(.03f);
             }
             else
             {
                 target.localScale += new Vector3(-1f, 0, 0);
                 target.localScale += new Vector3(0, 1f, 0);
+                yield return new WaitForSeconds(.03f);
             }
         }
-
-        yield return null;
     }
 }
